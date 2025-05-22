0889fe5ae35e0701e5cd13d135560d195d6dbb1b
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-22 10:19:35 +0700

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 import Container from "@/components/Container";
-import { Button } from "@/components/ui/button";
+import HomeBanner from "@/components/HomeBanner";
 
 const Home = () => {
   return (
     <Container className="bg-shop_light_pink">
-      <h2 className="text-xl font-semibold">Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
-        veniam beatae consectetur quod, inventore amet aliquid blanditiis et
-        mollitia repellendus vel neque accusantium aspernatur eligendi veritatis
-        rem laboriosam, enim explicabo asperiores alias omnis nulla officiis
-        porro fugiat? Blanditiis eligendi, dignissimos sit doloribus facere
-        numquam praesentium nisi doloremque porro, expedita repellat!
-      </p>
-      <Button size={"lg"}>Check out</Button>
+      <HomeBanner />
     </Container>
   );
 };
