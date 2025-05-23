10c0d044dfd1a7d4d80a85f11bf79171d8ed1c0d
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-23 11:16:27 +0700

    refactor: reorganize imports in Header component and adjust layout in HeaderMenu for better alignment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,19 @@
-import { currentUser } from "@clerk/nextjs/server";
-import CartIcon from "./CartIcon";
 import Container from "./Container";
-import FavoriteButton from "./FavoriteButton";
-import HeaderMenu from "./HeaderMenu";
 import Logo from "./Logo";
-import MobileMenu from "./MobileMenu";
+import HeaderMenu from "./HeaderMenu";
 import SearchBar from "./SearchBar";
+import CartIcon from "./CartIcon";
+import FavoriteButton from "./FavoriteButton";
 import SignIn from "./SignIn";
+import MobileMenu from "./MobileMenu";
+import { currentUser } from "@clerk/nextjs/server";
 import { ClerkLoaded, SignedIn, UserButton } from "@clerk/nextjs";
 
 const Header = async () => {
   const user = await currentUser();
 
   return (
-    <header className="bg-white/70 py-5 sticky top-0 z-50 backdrop-blur-md">
+    <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
       <Container className="flex items-center justify-between text-lightColor">
         <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
           <MobileMenu />
