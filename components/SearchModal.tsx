4ef3dbed3cf719240f482fa89aeba4234ab54bff
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-25 19:23:52 +0700

    feat: enhance SearchModal with loading state and improved search functionality

--- components/SearchModal.tsx
+++ components/SearchModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Search, X } from "lucide-react";
+import { useEffect, useState, useRef } from "react";
+import { Loader2, Search, X } from "lucide-react";
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import Link from "next/link";
 import Image from "next/image";
@@ -22,16 +22,29 @@ const SearchModal = ({
 }) => {
   const [search, setSearch] = useState("");
   const [filtered, setFiltered] = useState<Product[]>(products.slice(0, 7));
+  const [loading, setLoading] = useState(false);
+  const searchTimeout = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    if (search.trim() === "") {
-      setFiltered(products.slice(0, 7));
-    } else {
-      const result = products.filter((item) =>
-        item?.name?.toLowerCase().includes(search.toLowerCase())
-      );
-      setFiltered(result);
-    }
+    setLoading(true);
+
+    if (searchTimeout.current) clearTimeout(searchTimeout.current);
+
+    searchTimeout.current = setTimeout(() => {
+      if (search.trim() === "") {
+        setFiltered(products.slice(0, 7));
+      } else {
+        const result = products.filter((item) =>
+          item?.name?.toLowerCase().includes(search.toLowerCase())
+        );
+        setFiltered(result);
+      }
+      setLoading(false);
+    }, 500);
+
+    return () => {
+      if (searchTimeout.current) clearTimeout(searchTimeout.current);
+    };
   }, [search, products]);
 
   return (
@@ -63,87 +76,92 @@ const SearchModal = ({
             )}
             <Search className="ms-2 w-5 h-5 text-gray-400" />
           </div>
-          {search === "" ? (
-            <div className="flex items-center bg-gray-100 px-4 py-3 rounded text-sm text-gray-700 mb-4">
-              <Search className="w-4 h-4" />
-              <span className="ms-2 font-semibold">
-                Search and explore your products from
-              </span>
-              <Logo className="ms-1 text-sm" />
-            </div>
-          ) : filtered.length === 0 ? (
-            <div className="bg-gray-100 px-4 py-3 rounded text-sm text-gray-700 mb-4">
-              <span>
-                Nothing match with the keyword{" "}
-                <span className="text-red-600 underline font-semibold">
-                  {search}
-                </span>
-                . Please try something else.
-              </span>
-            </div>
-          ) : (
-            <div className="flex items-center bg-gray-100 px-4 py-3 rounded text-sm text-gray-700 mb-4">
-              <Search className="w-4 h-4" />
-              <span className="ms-2 font-semibold">
-                Search and explore your products from
-              </span>
-              <Logo className="ms-1 text-sm" />
+          {loading && (
+            <div className="flex justify-center items-center space-x-2 mb-4">
+              <Loader2 className="w-5 h-6 animate-spin" />
+              <p>Searching...</p>
             </div>
           )}
-          <ul className="space-y-2 max-h-[400px] overflow-y-auto">
-            {filtered.length > 0 &&
-              filtered.map((product) => (
-                <li key={product._id}>
-                  {search === "" ? (
-                    <button
-                      className="flex items-center gap-4 p-2 hover:bg-gray-100 rounded-lg w-full text-left"
-                      onClick={() => setSearch(product.name || "")}
-                    >
-                      <Search className="w-4 h-4 text-gray-400" />
-                      <span className="text-sm">{product.name}</span>
-                    </button>
-                  ) : (
-                    <div className="flex gap-4 p-3 border rounded-lg items-start relative w-full">
-                      <div className="w-20 h-20 relative shrink-0 rounded-md overflow-hidden border">
-                        {product.images?.[0] && (
-                          <Image
-                            src={urlFor(product.images[0]).url()}
-                            alt={product.name || "Product Image"}
-                            layout="fill"
-                            objectFit="cover"
-                            className="rounded-md"
-                          />
-                        )}
-                      </div>
-                      <div className="flex flex-col flex-1">
-                        <Link
-                          href={`/product/${product?.slug?.current}`}
-                          className="text-sm md:text-base font-medium text-gray-900 hover:underline"
-                          onClick={() => onOpenChange(false)}
+          {!loading && search === "" ? (
+            <div className="flex items-center bg-gray-100 px-4 py-3 rounded text-sm text-gray-700 mb-4">
+              <Search className="w-4 h-4" />
+              <span className="ms-2 font-semibold">
+                Search and explore your products from
+              </span>
+              <Logo className="ms-1 text-sm" />
+            </div>
+          ) : (
+            !loading &&
+            filtered.length === 0 && (
+              <div className="bg-gray-100 px-4 py-3 rounded text-sm text-gray-700 mb-4">
+                <span>
+                  Nothing match with the keyword{" "}
+                  <span className="text-red-600 underline font-semibold">
+                    {search}
+                  </span>
+                  . Please try something else.
+                </span>
+              </div>
+            )
+          )}
+          <div className="min-h-[400px] max-h-[400px] overflow-y-auto">
+            {!loading && (
+              <ul className="space-y-2">
+                {filtered.length > 0 &&
+                  filtered.map((product) => (
+                    <li key={product._id}>
+                      {search === "" ? (
+                        <button
+                          className="flex items-center gap-4 p-2 hover:bg-gray-100 rounded-lg w-full text-left"
+                          onClick={() => setSearch(product.name || "")}
                         >
-                          {product.name}
-                        </Link>
-                        <div className="mt-2">
-                          <AddToCartButton
-                            product={product}
-                            className="rounded-md w-36"
-                          />
-                        </div>
-                      </div>
-                      {product.price && (
-                        <div className="absolute top-3 right-3">
-                          <PriceView
-                            price={product.price}
-                            discount={product.discount}
-                            className="text-green-700 font-semibold text-sm"
-                          />
+                          <Search className="w-4 h-4 text-gray-400" />
+                          <span className="text-sm">{product.name}</span>
+                        </button>
+                      ) : (
+                        <div className="flex gap-4 p-3 border rounded-lg items-start relative w-full">
+                          <div className="w-20 h-20 relative shrink-0 rounded-md overflow-hidden border">
+                            {product.images?.[0] && (
+                              <Image
+                                src={urlFor(product.images[0]).url()}
+                                alt={product.name || "Product Image"}
+                                layout="fill"
+                                objectFit="cover"
+                                className="rounded-md"
+                              />
+                            )}
+                          </div>
+                          <div className="flex flex-col flex-1">
+                            <Link
+                              href={`/product/${product?.slug?.current}`}
+                              className="text-sm md:text-base font-medium text-gray-900 hover:underline"
+                              onClick={() => onOpenChange(false)}
+                            >
+                              {product.name}
+                            </Link>
+                            <div className="mt-2">
+                              <AddToCartButton
+                                product={product}
+                                className="rounded-md w-36"
+                              />
+                            </div>
+                          </div>
+                          {product.price && (
+                            <div className="absolute top-3 right-3">
+                              <PriceView
+                                price={product.price}
+                                discount={product.discount}
+                                className="text-green-700 font-semibold text-sm"
+                              />
+                            </div>
+                          )}
                         </div>
                       )}
-                    </div>
-                  )}
-                </li>
-              ))}
-          </ul>
+                    </li>
+                  ))}
+              </ul>
+            )}
+          </div>
         </div>
       </DialogContent>
     </Dialog>
